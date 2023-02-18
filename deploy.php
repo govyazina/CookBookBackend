8891edf9f52a8b2b401259bb0ecddc5d5339fb3c
Pavel Chaplygin <mail@pchapl.dev>
2023-02-18 21:33:09 +0200

    Update deploy: reload

--- deploy.php
+++ deploy.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Deployer;
 
 require 'recipe/symfony.php';
+require 'contrib/cachetool.php';
 
 // Config
 
@@ -14,12 +15,26 @@ add('shared_files', []);
 add('shared_dirs', []);
 add('writable_dirs', []);
 
+// PHP-FPM
+desc('PHP-FPM restart');
+task('fpm:restart', function () {
+    run('systemctl restart php8.2-fpm');
+});
+
 // Hosts
 
 host('138.197.188.157')
     ->set('remote_user', 'deployer')
-    ->set('deploy_path', '~/CookBookBackend');
+    ->set('deploy_path', '~/CookBookBackend')
+    ->set('cachetool', '/var/run/php/php-fpm.sock');
 
 // Hooks
 
 after('deploy:failed', 'deploy:unlock');
+
+after('deploy:symlink', 'cachetool:clear:opcache');
+after('deploy:symlink', 'fpm:restart');
+
+// [Optional] If deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
